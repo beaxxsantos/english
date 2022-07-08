b17ca4368ea32aca944cf9a96868a4fa89a3835b
beaxxsantos <beasantos.rj@outlook.com>
2022-07-08 15:57:50 -0300

    ingles

---
+++ script.js
@@ -0,0 +1,43 @@
+let movie = [
+
+    comedy = [
+       {
+       title:'White chicks',
+       director:'Keenen Ivory Wayans',
+       year:2004
+       },
+       {
+       title:'Dance Flick',
+       director:'Damian Dante Wayans',
+       year:2009
+       }
+   ],
+   
+    action = [
+       {
+       title:'Deadpool 2',
+       director:'Davi Leitch',
+       year:'2018'
+       },
+       {
+       title:'Fast and Furious',
+       director:'Robin Cohen',
+       year:'2001'
+       },
+   ],
+   
+   fantasy = [
+       {
+       title:'Lion King',
+       director:'Rob Minkoff, Roger Allers',
+       year:'1994'
+       }
+       ,{
+       title:'Descendants 3',
+       director:'Kenny Ortega',
+       year:'2019'
+       }
+   ] 
+   ]
+   
+   
